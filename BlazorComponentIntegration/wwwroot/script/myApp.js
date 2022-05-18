24523f650b40586c4d75000025a6522cd0bd65bf
danmihaipatrascu <danmihaipatrascu@gmail.com>
2022-05-18 12:06:48 +0300

    renaming; fixed redigest issue; added external component

--- BlazorComponentIntegration/wwwroot/script/myApp.js
+++ BlazorComponentIntegration/wwwroot/script/myApp.js
@@ -1,15 +1,21 @@
 ﻿
+var initIndex = 0;
+
 function reinitAngular() {
     console.log('reinitAngular called');
+
     var injector = $('[ng-app]').injector();
     var $compile = injector.get('$compile');
     var $rootScope = injector.get('$rootScope');
     var appElement = $('[ng-app]');
     $compile(appElement)($rootScope);
+
+    //digest should be done only the first time
+    if (initIndex > 0)
+        return;
+    initIndex++;
     $rootScope.$digest();
 }
-
-
 
 var app = angular.module('myApp', ['ngRoute']);
 app.controller('myCtrl', function ($scope) {
@@ -27,7 +33,12 @@ app.config(function ($routeProvider, $locationProvider) {
         .when('/secondPage', {
             templateUrl: 'routedemo/second.html',
             controller: 'routeDemoSecondController'
-        });
+        })
+        .when('/thirdPage', {
+            templateUrl: 'routedemo/third.html',
+            controller: 'routeDemoThirdController'
+        })
+        .otherwise({ redirectTo: '/firstPage' });
 
     //$locationProvider.html5Mode(true);
 })  
@@ -41,4 +52,8 @@ app.controller("routeDemoSecondController", function ($scope) {
     console.log('routeDemoSecondController');
     $scope.MobileNumber = "123456";
     $scope.EmailID = "anu@test.com";
+})
+app.controller("routeDemoThirdController", function ($scope) {
+    console.log('routeDemoThirdController');
+    $scope.XXX = "123456";
 })  
